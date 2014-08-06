bebc45a74035142f8f16ecc5d04a8abb9abbbfc8
justin-lilly <jlilly353@gmail.com>
2014-08-06 12:33:01 -0500

    Add table creation

---
+++ db/dbi.rb
@@ -0,0 +1,56 @@
+require 'pg'
+
+module Database
+    class DBI
+     
+        # this initialize method is only ever run once. make sure you
+        # update your `dbname`. here it is petbreeder.
+        def initialize
+          @db = PG.connect(host: 'localhost', dbname: 'killer_rps')
+          build_tables
+        end
+        
+        # these methods create the tables in your db if they
+        # dont already exist
+        def build_tables
+          @db.exec(%q[
+            CREATE TABLE IF NOT EXISTS users(
+                id serial NOT NULL PRIMARY KEY,
+                name varchar(30),
+                password_digest integer,
+                email text,
+                last_login timestamp,
+                game_history integer[],
+                created_at timestamp NOT NULL DEFAULT current_timestamp
+            )])
+
+            @db.exec(%q[
+            CREATE TABLE IF NOT EXISTS games(
+                id serial NOT NULL PRIMARY KEY,
+                player1_id integer REFERENCES users(id),
+                player2_id integer REFERENCES users(id),
+                game_winner_id integer REFERENCES users(id),
+                match_list integer[],
+                created_at timestamp NOT NULL DEFAULT current_timestamp
+            )])
+
+            @db.exec(%q[
+            CREATE TABLE IF NOT EXISTS matches(
+                id serial NOT NULL PRIMARY KEY,
+                player1_id integer REFERENCES users(id),
+                player2_id integer REFERENCES users(id),
+                player1_move varchar(9),
+                player2_move varchar(9),
+                player1_result varchar(5),
+                player2_result varchar(5),
+                created_at timestamp NOT NULL DEFAULT current_timestamp
+            )])
+        end
+
+
+    end
+
+    def self.dbi
+      @__db_instance ||= DBI.new
+    end
+end
